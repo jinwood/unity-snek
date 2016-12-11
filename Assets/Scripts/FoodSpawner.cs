bb002e88242e14ca51dcbddb882eaae0ff247768
Julian Inwood <jinwood8@outlook.com>
2016-12-11 22:24:17 +0000

    Add basic game area, borders, snake prefab, food prefab, food controller, snake movement script.

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawner : MonoBehaviour
+{
+
+	public GameObject food;
+	public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+	private int maxFood = 5;
+	private int foodCounter = 0;
+
+
+	void Update ()
+	{
+		if (foodCounter < maxFood) {
+			foodCounter++;
+			Spawn ();
+		}
+	}
+
+	void Spawn ()
+	{
+		int x = (int)Random.Range (borderLeft.position.x, borderRight.position.x);
+		int y = (int)Random.Range (borderTop.position.y, borderBottom.position.y);
+
+		Instantiate (food, new Vector2 (x, y), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snake : MonoBehaviour
+{
+	Vector2 dir = Vector2.right;
+
+	void Start ()
+	{
+		InvokeRepeating ("Move", 0.3f, 0.3f);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey ("w")) {
+
+			dir = Vector2.up;
+		} else if (Input.GetKey ("a")) {
+			dir = Vector2.left;
+		} else if (Input.GetKey ("s")) {
+			dir = Vector2.down;
+		} else if (Input.GetKey ("d")) {
+			dir = Vector2.right;
+		}
+			
+	}
+
+	void Move ()
+	{
+		transform.Translate (dir);
+	}
+
+
+}
